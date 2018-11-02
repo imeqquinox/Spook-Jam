36c04063e48c932d3f0c64ab61e7b11b608992b2
Lewis <lewiswilson1998@hotmail.co.uk>
2018-11-02 20:06:51 +0000

    Small changes
    Player movement and scene change

---
+++ Tower of Horrors/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed = 5;
+
+    private Rigidbody rb; 
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>(); 
+	}  
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor, 0.0f, moveVer) * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + movement);
+	}
+}
