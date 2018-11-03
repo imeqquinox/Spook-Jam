3cb47882e2c733be05d0860955cce069c57b2fa2
Glitch99 <scottafjarvis@gmail.com>
2018-11-03 17:54:14 +0000

    Changes made on Day One

---
+++ Tower of Horrors/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+    void FixedUpdate()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10f;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 100, 0), ForceMode.Impulse);
+        }
+
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+    }
+
+}
